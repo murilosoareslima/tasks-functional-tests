c7c399ff864234fe75020f5828a643bdcbdd2787
Murilo <murilosoareslima@gmail.com>
2022-02-16 19:03:06 -0300

    teste de remoção

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -113,4 +113,34 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
+
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		WebDriver driver = acessarAplicacao();
+		try {
+			// encontrar o bot�o por id na p�gina inicial e clicar
+			driver.findElement(By.id("addTodo")).click();
+
+			// encontrar primeiro campo para escrever
+			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+
+			// encontrar segundo campo para escrever
+			driver.findElement(By.id("dueDate")).sendKeys("10/01/2023");
+
+			// clicar no bot�o salvar
+			driver.findElement(By.id("saveButton")).click();
+
+			// validar a mensagem que aparece na tela
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+
+			//remove tarefa
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm']")).click();
+			message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			// fechar o brownser
+			driver.quit();
+		}
+	}
 }
